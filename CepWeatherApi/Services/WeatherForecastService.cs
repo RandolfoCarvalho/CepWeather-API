feeae9c3ebe0240930f7ab5b73c75cfd00303678
RandolfoCarvalho <randolfoirades@gmail.com>
2023-12-30 12:54:49 -0300

    passes the correct data to the api

--- CepWeatherApi/Services/WeatherForecastService.cs
+++ CepWeatherApi/Services/WeatherForecastService.cs
@@ -13,23 +13,17 @@ public class WeatherForecastService
 {
     private readonly HttpClient _httpClient;
     private readonly CepWeatherApiContext _context;
+
     public WeatherForecastService(HttpClient httpClient, CepWeatherApiContext context)
     {
         _httpClient = httpClient;
         _context = context;
-
     }
     public List<Weather> FindAll()
     {
         return _context.Weather.ToList();
     }
 
-    /*public async Task<Weather> FindById(int id)
-    {
-        //da um join para buscar também o departamento pelo Id
-        //isso é chamado de eager loading, carregar outros objetos associados ao obj principal, que no caso o princial 
-        return await _context.Weather.FirstOrDefaultAsync(obj => obj.Id == id);
-    } */
     public  Weather FindById(int id)
     {
         var result = _context.Weather.FirstOrDefault(p => p.Id == id);
@@ -58,7 +52,7 @@ public class WeatherForecastService
             throw new DBConcurrencyException("Erro de DbConcurrency " + e.Message);
         }
     }
-   
+
     public async void Delete(Weather weather)
     {
         _context.Remove(weather);
@@ -71,7 +65,7 @@ public class WeatherForecastService
         string fimData = fim.ToString("yyyy-MM-dd");
 
         string apiUrl = $"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=" +
-            $"temperature_2m&timezone=America/Sao_Paulo&start_date=2023-07-01&end_date=2023-07-02";
+            $"temperature_2m&timezone={timezone}&start_date={inicioData}&end_date={fimData}";
 
         var response = await _httpClient.GetStringAsync(apiUrl);
         return response;
